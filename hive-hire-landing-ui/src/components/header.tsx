5ca06bbf28c179ee93026973418f379ce2aba78c
Victor Assis <victor_assis@poli.ufrj.br>
2024-03-02 16:14:11 -0300

    REFAC: Platform button

--- hive-hire-landing-ui/src/components/header.tsx
+++ hive-hire-landing-ui/src/components/header.tsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
-
 import logo from '@/assets/logo.svg'
 import { Button } from '@/components/ui/button'
 
 import { NavMenu } from './navigation/nav-menu'
 import { Separator } from './ui/separator'
+import { DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from './ui/dialog'
 import { env } from '@/env'
 
 export function Header() {
@@ -21,12 +21,99 @@ export function Header() {
         <NavMenu />
 
         <div className="flex gap-2">
-          <Button>
-            <Link to={env.VITE_PLATFORM_URL}>Open User Platform</Link>
-          </Button>
+          <DialogTrigger asChild>
+            <Button>
+              Open Platform
+            </Button>
+          </DialogTrigger>
+          <DialogContent className='grid grid-cols-2'>
+            <div className='flex flex-col relative'>
+              <DialogHeader>
+                <DialogTitle>Open as client
+
+                <span className='opacity-70 text-destructive text-xs'> comming soon...</span>
+
+                </DialogTitle>
+                <DialogDescription>
+                  <ul className='list-disc ml-4'>
+                    <li>Hire new equipment</li>
+                    <li>Manage hired assets</li>
+                    <li>Open support tickets</li>
+                    <li>And more...</li>
+                  </ul>
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <Button disabled={true}>Open as client</Button>
+              </DialogFooter>
+            <Separator orientation='vertical' className='absolute -right-2'/>
+            </div>
+            <div className='flex flex-col'>
+              <DialogHeader>
+                <DialogTitle>Open as admin</DialogTitle>
+                <DialogDescription>
+                  <ul className='list-disc ml-4'>
+                    <li>Manage hired assets</li>
+                    <li>Manage support tickets</li>
+                    <li>Manage new orders</li>
+                    <li>And more...</li>
+                  </ul>
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <Button>
+                  <Link to={env.VITE_PLATFORM_URL}>Open as admin</Link>
+                </Button>
+              </DialogFooter>
+            </div>
+            {/* <div className='flex justify-between'>
+              <div className='flex flex-col w-full items-center'>
+                <DialogHeader>
+                  <DialogTitle>Open as Client</DialogTitle>
+                  <DialogDescription>
+                    <ul className='list-disc'>
+                      <li>Hire new equipment</li>
+                      <li>Manage hired assets</li>
+                      <li>Open support tickets</li>
+                      <li>And more...</li>
+                    </ul>
+                  </DialogDescription>
+                </DialogHeader>
+                
+              </div>
+            <Separator orientation='vertical' />
+              <div className='flex flex-col w-full items-center'>
+                <DialogHeader>
+                  <DialogTitle>Open as Admin</DialogTitle>
+                  <DialogDescription>
+                  <ul className='list-disc'>
+                      <li>Manage hired assets</li>
+                      <li>Manage support tickets</li>
+                      <li>Manage new orders</li>
+                      <li>And more...</li>
+                    </ul>
+                  </DialogDescription>
+                </DialogHeader>
+              </div>
+            </div>
+
+            <DialogFooter className='flex justify-between'>
+              <div className='flex w-full justify-center flex-col'>
+                <div className='flex justify-center'>
+                  <Button disabled={true}>Open as client</Button>
+                </div>
+              </div>
+              <div className='flex flex-col w-full justify-center'>
+                <div className='flex justify-center'>
+
+              <Button className='mt-4'>Open as admin</Button>
+                </div>
+              </div>
+            </DialogFooter>
+          <Separator /> */}
+          </DialogContent>
         </div>
       </div>
-      <Separator />
     </>
   )
 }
--- hive-hire-landing-ui/src/pages/_layouts/landing.tsx
+++ hive-hire-landing-ui/src/pages/_layouts/landing.tsx
@@ -2,9 +2,11 @@ import { Outlet } from 'react-router-dom'
 
 import { Header } from '@/components/header'
 import { Separator } from '@/components/ui/separator'
+import { Dialog } from '@/components/ui/dialog'
 
 export function Landing() {
   return (
+    <Dialog>
       <div className="m-auto max-w-screen-2xl">
         <Header />
         <div className="m-auto flex max-w-screen-xl flex-1 flex-col gap-4 p-8 pt-6">
@@ -17,5 +19,6 @@ export function Landing() {
           </span>
         </div>
       </div>
+    </Dialog>
   )
 }
